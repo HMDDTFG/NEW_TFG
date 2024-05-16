19d48fbd004e6c815acac4295a3a962f02359d34
HMDDTFG <168761694+HMDDTFG@users.noreply.github.com>
2024-05-16 20:02:47 +0200

    Segundo Commit
    El sorting de los precios funciona pero sin Base de Datos.

--- index.js
+++ index.js
@@ -20,5 +20,31 @@ function FUNCION(){
 	var header = document.querySelector('header');
 	header.classList.toggle("CLASE", window.scrollY > 5);
 }
-
 // window.addEventListener('scroll',FUNCION);
+
+$(document).ready(function() {
+    $(".price-sorting-link").on("click", function(event) {
+        event.preventDefault();
+        var sortingMethod = $(this).data("sort");
+
+        if(sortingMethod == 'l2h') {
+            sortProducts(true);
+        } else if(sortingMethod == 'h2l') {
+            sortProducts(false);
+        }
+    });
+
+    function sortProducts(asc) {
+        var products = $('.producto').sort(function(a, b) {
+            if(asc)
+                return parseFloat($(a).data("price")) - parseFloat($(b).data("price"));
+            return parseFloat($(b).data("price")) - parseFloat($(a).data("price"));
+        });
+        
+        $(".products-grid").html(); //Vacía el contenido HTML.
+        products.each((i) => {
+            console.log(products.eq(i).parent());
+            $(".products-grid").append(products.eq(i).parent()); //Añade los productos a ese HTML vacio de uno en uno que es un bucle.
+        })
+    }
+});
